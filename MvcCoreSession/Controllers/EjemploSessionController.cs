a54a2218b8e1d1c2c101493fb35a27b6b5428775
mmtol <mmigueltolosana@gmail.com>
2026-02-18 11:52:03 +0100

    aaaaa

--- MvcCoreSession/Controllers/EjemploSessionController.cs
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreSession.Helpers;
+using MvcCoreSession.Models;
 
 namespace MvcCoreSession.Controllers
 {
@@ -27,6 +29,82 @@ namespace MvcCoreSession.Controllers
                     ViewData["Hora"] = HttpContext.Session.GetString("hora");
                 }
             }
+            return View();
+        }
+
+        public IActionResult SessionMascotaBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //guardamos datos en session
+                    Mascota mascota = new Mascota();
+                    mascota.Nombre = "Goofy";
+                    mascota.Raza = "Perro";
+                    mascota.Edad = 10;
+
+                    byte[] data = HelperBinarySession.ObjetcToByte(mascota);
+                    //almacenamos el obj en session
+                    HttpContext.Session.Set("Mascota", data);
+                    ViewData["Mensaje"] = "Mascota almacenada en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //recuperamos los datos de session
+                    byte[] data = HttpContext.Session.Get("Mascota");
+                    Mascota mascota = (Mascota)HelperBinarySession.ByteToObject(data);
+                    //para representarlo de forma visuañ, lo enviamos a viewdata
+                    ViewData["Mascota"] = mascota;
+                }
+            }
+            return View();
+        }
+
+        public IActionResult SessionMascotaColectionBytes(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //guardamos datos en session
+                    Mascota mascota1 = new Mascota
+                    {
+                        Nombre = "Goofy",
+                        Raza = "Perro",
+                        Edad = 10
+                    };
+                    Mascota mascota2 = new Mascota
+                    {
+                        Nombre = "Pluto",
+                        Raza = "Perro",
+                        Edad = 9
+                    };
+                    Mascota mascota3 = new Mascota
+                    {
+                        Nombre = "Groot",
+                        Raza = "Perro",
+                        Edad = 7
+                    };
+
+                    List<Mascota> mascotas = new List<Mascota>
+                    {
+                        mascota1, mascota2, mascota3
+                    };
+
+                    byte[] data = HelperBinarySession.ObjetcToByte(mascotas);
+                    HttpContext.Session.Set("Mascotas", data);
+                    ViewData["Mensaje"] = "Coleccion almacenada correctamente";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //recuperamos los datos de session
+                    byte[] data = HttpContext.Session.Get("Mascotas");
+                    List<Mascota> mascotas = (List<Mascota>)HelperBinarySession.ByteToObject(data);
+                    return View(mascotas);
+                }
+            }
+
             return View();
         }
     }
---
+++ MvcCoreSession/Helpers/HelperBinarySession.cs
@@ -0,0 +1,39 @@
+﻿
+
+
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace MvcCoreSession.Helpers
+{
+    #pragma warning disable SYSLIB0011
+
+    public class HelperBinarySession
+    {
+        //vamos a crear los metodos de tipo static 
+        //porque para convertir no voy a utilizar nada de la clase, solo la funcionalidad
+
+        //convertimos objeto a byte[]
+        public static byte[] ObjetcToByte(Object obj)
+        {
+            BinaryFormatter form = new BinaryFormatter();
+            using (MemoryStream stream = new MemoryStream())
+            {
+                form.Serialize(stream, obj);
+                return stream.ToArray();
+            }
+        }
+
+        //convertimos de byte[] a obj
+        public static Object ByteToObject(byte[] data)
+        {
+            BinaryFormatter form = new BinaryFormatter();
+            using(MemoryStream stream= new MemoryStream())
+            {
+                stream.Write(data, 0, data.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+                Object obj = (Object)form.Deserialize(stream);
+                return obj;
+            }
+        }
+    }
+}
