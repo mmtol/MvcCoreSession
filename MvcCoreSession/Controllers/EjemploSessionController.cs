f1fb5927625fe2824ff83e2672d77f82df0cec72
mmtol <mmigueltolosana@gmail.com>
2026-02-18 09:48:13 +0100

    Add project files.

---
+++ MvcCoreSession/Controllers/EjemploSessionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcCoreSession.Controllers
+{
+    public class EjemploSessionController : Controller
+    {
+        public IActionResult SessionSimple(string accion)
+        {
+            if (accion != null)
+            {
+                if (accion.ToLower() == "almacenar")
+                {
+                    //guardamos datos en session
+                    HttpContext.Session.SetString("nombre", "Dev");
+                    HttpContext.Session.SetString("hora", DateTime.Now.ToLongTimeString());
+                    ViewData["Mensaje"] = "Datos almacenados en Session";
+                }
+                else if (accion.ToLower() == "mostrar")
+                {
+                    //recuperamos los datos de session
+                }
+            }
+            return View();
+        }
+    }
+}
